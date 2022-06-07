37fc620da20660b5606a0a9323b39c8c135eb2ae
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-06-07 14:29:32 +0300

    правки

--- task_5/task_5.cpp
+++ task_5/task_5.cpp
@@ -11,8 +11,46 @@
 */
 
 #include <iostream>
+#include <windows.h>
+#include <io.h>
+
+using namespace std;
 
 int main()
 {
+	SetConsoleCP(1251);
+	/*
+	Задает кодовую страницу ввода, используемую консолью, связанной с вызывающим процессом.
+	Консоль использует страницу входного кода для преобразования ввода с клавиатуры
+	в соответствующее символьное значение.
+	*/
+	SetConsoleOutputCP(1251);
+	/*
+	Задает выходную кодовую страницу, используемую консолью, связанной с вызывающим процессом.
+	Консоль использует свою кодовую страницу вывода для преобразования символьных значений,
+	записанных различными функциями вывода, в изображения, отображаемые в окне консоли.
+	*/
 
+	char fileNameRead[MAX_PATH];
+	cout << "Введите путь к файлу для шифрования: ";
+	cin.getline(fileNameRead, MAX_PATH);
+	FILE* f_read{ nullptr };
+	fopen_s(&f_read, fileNameRead, "r");
+	int fileLength = _filelength(_fileno(f_read)); // _fileno (получает дескриптор), _filelength (дает размер файла в байтах)
+	char* bufferForFile = new char[fileLength]; 
+	char key ;
+	cout << "Введите ключ для шифрования: ";
+	cin>>key;
+	for (int i = 0; i < fileLength; i++)
+	{
+		for (size_t j = 0; j < sizeof(key); j++)
+		{
+			bufferForFile[i] = bufferForFile[i] ^ key[j];
+			i++;
+		}
+	}
+
+
+
+	delete[]bufferForFile;
 }
--- task_6/task_6.cpp
+++ task_6/task_6.cpp
@@ -1,18 +1,111 @@
-п»ї/*
-	6. РќР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ, РєРѕС‚РѕСЂР°СЏ СЃС‡РёС‚С‹РІР°РµС‚ С‚РµРєСЃС‚ РёР· С„Р°Р№Р»Р° Рё
-РІС‹РІРѕРґРёС‚ РІ РґСЂСѓРіРѕР№ С„Р°Р№Р» СЃР»РѕРІР° РёР· СЌС‚РѕРіРѕ С‚РµРєСЃС‚Р°, СЂР°СЃРїРѕР»РѕР¶РёРІ РёС…
-РІ РїРѕСЂСЏРґРєРµ СѓР±С‹РІР°РЅРёСЏ С‡Р°СЃС‚РѕС‚С‹ РёС… РїРѕСЏРІР»РµРЅРёСЏ. РџСЂРё СЌС‚РѕРј РґР»СЏ
-РєР°Р¶РґРѕРіРѕ СЃР»РѕРІР° РЅРµРѕР±С…РѕРґРёРјРѕ РЅР°РїРµС‡Р°С‚Р°С‚СЊ С‡РёСЃР»Рѕ РµРіРѕ РїРѕСЏРІР»РµРЅРёР№
-РІ РёСЃС…РѕРґРЅРѕРј С„Р°Р№Р»Рµ.
+/*
+	6. Написать программу, которая считывает текст из файла и
+выводит в другой файл слова из этого текста, расположив их
+в порядке убывания частоты их появления. При этом для
+каждого слова необходимо напечатать число его появлений
+в исходном файле.
 
-РџСѓС‚СЊ Рє РІС…РѕРґРЅРѕРјСѓ/РІС‹С…РѕРґРЅРѕРјСѓ С„Р°Р№Р»Сѓ Р·Р°РґР°РµС‚ РїРѕР»СЊР·РѕРІР°С‚РµР»СЊ !
-
-РќР• Р РђР‘РћРўРђР•Рў !!!
+Путь к входному/выходному файлу задает пользователь !
+3.10.32
+НЕ РАБОТАЕТ !!!
 */
 
 #include <iostream>
+#include <windows.h>
+#include <io.h>
+using namespace std;
+
+void RussianMessage(const char* str)
+{
+	char* p = new char[strlen(str) + 1];
+	CharToOemA(str, p);
+	cout << p;
+	delete[] p;
+}
+
+struct FIleStat
+{
+	char* word{ nullptr };
+	int* count{ nullptr };
+
+}*fileStat;
 
 int main()
 {
+	FIleStat* arrStruct = new FIleStat[1000];
 
+	char fileNameRead[MAX_PATH];
+	cout << "Enter path name: ";
+	cin.getline(fileNameRead, MAX_PATH);
+	FILE* f_read{ nullptr };
+	fopen_s(&f_read, fileNameRead, "r");
+	if (f_read == nullptr)
+	{
+		perror("Error opening");
+		return 0;
+	}
+	char buffer_for_string[1000];
+	char* words[1000]{ nullptr };
+	char seps[] = " ,\t\n";
+	char* token = nullptr;
+	char* next_token = nullptr;
+
+
+
+	/*
+	Попытка сичтать текстовый файл целиком, построчно
+	char* buffer_for_stringTest = new char[1000];
+	int lenght = _filelength(_fileno(f_read)); // _fileno (получает дескриптор), _filelength (дает размер файла в байтах)
+	char* bufForAllFIle = new char[lenght]; 
+	memset(bufForAllFIle, 0, lenght);
+	while (!feof(f_read))
+	{
+		fgets(buffer_for_stringTest, 1000, f_read);
+		strcat_s(bufForAllFIle,strlen(buffer_for_stringTest)+1, buffer_for_stringTest);
+	}
+	*/
+	FILE* f1 = nullptr;
+	fopen_s(&f1, fileNameRead, "rb");
+	if (f1 == nullptr)
+	{
+		perror("Error opening");
+		return 0;
+	}
+	int lenght = _filelength(_fileno(f1)); // _fileno (получает дескриптор), _filelength (дает размер файла в байтах)
+	char* bufForDataFile = new char[lenght]; // буфер на весь файл
+	memset(bufForDataFile, 0, lenght);
+	fread(bufForDataFile, sizeof(char), lenght, f1);
+	cout << bufForDataFile << endl;
+
+
+
+
+
+	int i{ 0 };
+	while (!feof(f_read))
+	{
+		fgets(buffer_for_string, 1000, f_read);
+
+		// Establish string and get the first token:
+		token = strtok_s(buffer_for_string, seps, &next_token); // "вырезаем" первое слово, передаем остаток дальше
+		words[i] = token;
+		i++;
+		// While there are tokens in "string1" or "string2"
+		while (token != nullptr)
+		{
+			// Get next token:
+			if (token != nullptr)
+			{
+				token = strtok_s(nullptr, seps, &next_token);
+				if (token) {
+					words[i] = token;
+					i++;
+
+				}
+			}
+			//место обработки данных из массива пока его не затерли новой строкой
+		}
+	}
+	delete[]arrStruct;
+	fclose(f_read);
 }
